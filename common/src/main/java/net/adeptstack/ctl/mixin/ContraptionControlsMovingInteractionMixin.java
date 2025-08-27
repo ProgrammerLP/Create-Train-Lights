5a2ef2dbf31880df948a85a36ba47e84de263eb8
ProgrammerLP <developer.programmerlp@gmx.net>
2025-08-27 22:06:35 +0200

    fixed: glue is not working correct

--- common/src/main/java/net/adeptstack/ctl/mixin/ContraptionControlsMovingInteractionMixin.java
+++ common/src/main/java/net/adeptstack/ctl/mixin/ContraptionControlsMovingInteractionMixin.java
@@ -18,6 +18,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.StructureBlockInfo;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.fml.ISystemReportExtender;
 import org.apache.commons.lang3.tuple.MutablePair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -33,11 +34,10 @@ public class ContraptionControlsMovingInteractionMixin {
     private void send(AbstractContraptionEntity contraptionEntity, ItemStack filter, boolean disable) { throw new AssertionError(); }
 
     @Inject(method = "handlePlayerInteraction", remap = false, at = @At(value = "TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void customHandlePlayerInteraction(Player player, InteractionHand activeHand, BlockPos localPos,
-                                               AbstractContraptionEntity contraptionEntity, CallbackInfoReturnable<Boolean> cir,
-                                               Contraption contraption, MutablePair<StructureBlockInfo, MovementContext> actor,
-                                               MovementContext ctx, ItemStack filter, boolean disable) {
+    private void customHandlePlayerInteraction(Player player, InteractionHand activeHand, BlockPos localPos, AbstractContraptionEntity contraptionEntity, CallbackInfoReturnable<Boolean> cir, Contraption contraption, MutablePair<StructureBlockInfo, MovementContext> actor, MovementContext ctx, ItemStack filter, boolean disable) {
         if (contraptionEntity instanceof CarriageContraptionEntity cce) {
+            System.out.println("Hallo");
+            System.out.println(filter.getTag());
             if (filter.is(ItemTags.DOORS)) {
                 Carriage carriage = cce.getCarriage();
                 Train train = carriage.train;
